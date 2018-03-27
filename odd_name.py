3a1acc1a617cb69695b60686534962d53d420d70
Jes <jesbarlow42@gmail.com>
2018-03-27 20:49:17 +1000

    if applied this commit will add a name printing program that is designed to print every second letter.

--- odd_name.py
+++ odd_name.py
@@ -2,3 +2,11 @@
 Jessica Barlow
 """
 
+while True:
+    name = input("What is your name?: ")
+    if name.isalpha():
+        break
+    else:
+        print("Sorry, i didn't understand that.")
+
+print (name[::2])
